123a5821af2db8b7fa95498c0c086c25ef7c0351
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2020-12-31 21:00:07 -0500

    Update DateTimeGetter.cs

--- CreditManager.API/Domain/Helpers/DateTimeGetter.cs
+++ CreditManager.API/Domain/Helpers/DateTimeGetter.cs
@@ -8,7 +8,8 @@ namespace CreditManager.API.Domain.Helpers
     public class DateTimeGetter
     {
         public static DateTime GetDateTimeFromPeru()
-        {
+        {// TODO: Ver sobre obtener la hora de peru, incluso cuando la app este alojada en USA o Europa
+            //https://docs.microsoft.com/es-es/dotnet/standard/datetime/converting-between-time-zones
             return DateTime.Now;
         }
     }
